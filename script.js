d71c5c248403c061bcf57d8b80579f7b313d2a08
sofatukenova884-png <sofatukenova884@gmail.com>
2026-03-19 08:41:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,333 @@
+const data = {
+ "sources":[
+   {
+     "id": 1,
+     "name": "meduza"
+   },
+   {
+     "id": 6,
+     "name": "3dnews"
+   },
+   {
+     "id": 7,
+     "name": "nytimes"
+   },
+   {
+     "id": 8,
+     "name": "forbes"
+   },
+   {
+     "id": 9,
+     "name": "igromania"
+   },
+   {
+     "id": 10,
+     "name": "buro237"
+   },
+   {
+     "id": 11,
+     "name": "rusvesna"
+   },
+   {
+     "id": 12,
+     "name": "7ya"
+   },
+   {
+     "id": 14,
+     "name": "aif"
+   },
+   {
+     "id": 15,
+     "name": "gazetaru"
+   },
+   {
+     "id": 16,
+     "name": "karpov.courses"
+   }
+],
+ "categories":[
+   {
+     "id": 1,
+     "name": "tech"
+   },
+   {
+     "id": 2,
+     "name": "sport"
+   },
+   {
+     "id": 3,
+     "name": "fashion"
+   },
+   {
+     "id": 4,
+     "name": "politics"
+   },
+   {
+     "id": 5,
+     "name": "other"
+   },
+   {
+     "id": 6,
+     "name": "karpov.courses"
+   }
+ ],
+ "items":[
+   {
+     "id": 87901,
+     "lang": "ru",
+     "date": "Mon Oct 11 2021 16:58:58 GMT+0300 (Moscow Standard Time)",
+     "title": "Omoda C5 получит в России новую базовую версию Fun",
+     "description": "Как сообщает пресс-служба компании, старт продаж автомобиля в начальной комплектации намечен на апрель 2024 года. Цены на новую модификацию пока не называются.",
+     "image": "https://avatars.mds.yandex.net/get-autoru-vos/1681159/ffb3f547051b2577d912626f53e8effc/1200x900",
+     "source_id": 15,
+     "category_id": 4
+   },
+   {
+     "id": 87874,
+     "lang": "ru",
+     "date": "Mon Oct 11 2021 16:54:23 GMT+0300 (Moscow Standard Time)",
+     "title": "«УАЗ возобновил производство «экспедиционных» версий своих моделей.",
+     "description": "Первыми с конвейера начали сходить автомобили СГР («Буханка») и «Хантер» в версиях «Экспедиция». Прием заказов на эти модели будет открыт в апреле. Цены стартуют от 1 690 000 и 1 785 000 рублей соответственно.",
+     "image": "https://avatars.mds.yandex.net/i?id=e2e3d4cb72a5396696179582e97b5cd1_l-11540573-images-thumbs&n=13",
+     "source_id": 14,
+     "category_id": 4
+   },
+   {
+     "id": 87926,
+     "lang": "ru",
+     "date": "Mon Oct 11 2021 16:53:22 GMT+0300 (Moscow Standard Time)",
+     "title": "АвтоВАЗ запустил производство Lada в Азербайджане.",
+     "description": "В АвтоВАЗе рассказали, что на одной из местных производственных площадок организованы зона приема машинокомплектов, сборочный цех и стоянка хранения готовых автомобилей. ",
+     "image": "https://avatars.mds.yandex.net/i?id=dee628c9e2f0b4576e550806e7f00afa_l-12538721-images-thumbs&n=13",
+     "source_id": 15,
+     "category_id": 1
+   },
+   {
+     "id": 87867,
+     "lang": "ru",
+     "date": "Mon Oct 11 2021 16:48:54 GMT+0300 (Moscow Standard Time)",
+     "title": "АвтоВАЗ начал продажи Lada Niva Bronto с ABS и двигателем «Евро-5».",
+     "description": "Специальный указ был подписан главой ЛНР.",
+     "image": "https://rusvesna.su/sites/default/files/gerb_lnr_simvolika.jpg",
+     "source_id": 11,
+     "category_id": 5
+   },
+   {
+     "id": 87914,
+     "lang": "ru",
+     "date": "Mon Oct 11 2021 16:48:32 GMT+0300 (Moscow Standard Time)",
+     "title": "Электромобиль Xiaomi SU7, продажи которого начались 28 марта, собрал 50 тыс. предзаказов за 27 минут",
+     "description": "Электромобиль Xiaomi SU7, продажи которого начались 28 марта, собрал 50 тыс. предзаказов за 27 минут",
+     "image": "https://img.gazeta.ru/files3/938/13995938/AP101029115546-pic905-895x505-15444.jpg",
+     "source_id": 15,
+     "category_id": 4
+   },
+   {
+     "id": 87865,
+     "lang": "ru",
+     "date": "Mon Oct 11 2021 16:48:00 GMT+0300 (Moscow Standard Time)",
+     "title": "Водителям в Москве порекомендовали поменять зимнюю резину на летнюю.",
+     "description": "Водителям в Москве порекомендовали поменять зимнюю резину на летнюю.",
+     "image": "https://cdn.igromania.ru/mnt/news/b/5/2/6/e/b/109638/50159a4e1b392974_848x477.jpg",
+     "source_id": 9,
+     "category_id": 1
+   },
+   {
+     "id": 87870,
+     "lang": "ru",
+     "date": "Mon Oct 11 2021 16:45:05 GMT+0300 (Moscow Standard Time)",
+     "title": "Белорусский электрокар Belgee eX50 пообещали запустить до конца года. ",
+     "description": "Белорусский электрокар Belgee eX50 пообещали запустить до конца года. ",
+     "image": "https://www.buro247.ru/images/andreeva/670x340_cardi1110.jpg",
+     "source_id": 10,
+     "category_id": 5
+   },
+   {
+     "id": 87880,
+     "lang": "ru",
+     "date": "Mon Oct 11 2021 16:44:18 GMT+0300 (Moscow Standard Time)",
+     "title": "С 1 апреля в России ужесточили правила ввоза автомобилей по параллельному импорту",
+     "description": "С 1 апреля в России ужесточили правила ввоза автомобилей по параллельному импорту",
+     "image": "https://aif-s3.aif.ru/images/025/372/d3e7fd239323d0973ae5db8ee7f52640.jpg",
+     "source_id": 14,
+     "category_id": 5
+   },
+   {
+     "id": 87869,
+     "lang": "ru",
+     "date": "Mon Oct 11 2021 16:44:05 GMT+0300 (Moscow Standard Time)",
+     "title": "Суд признал законной блокировку ютьюб-канала и сайта Навального",
+     "description": "Тверской суд Москвы отклонил иск Алексея Навального к Генпрокуратуре РФ и Роскомнадзору из-за блокировки ютьюб-канала «Навальный Live» и сайта «Свободу Навальному». Об этом сообщает «Интерфакс» со ссылкой на пресс-службу суда.",
+     "image": "",
+     "source_id": 1,
+     "category_id": 4
+   },
+   {
+     "id": 87903,
+     "lang": "ru",
+     "date": "Mon Oct 11 2021 16:43:54 GMT+0300 (Moscow Standard Time)",
+     "title": "Jetour в Китае начал продажи гибридной версии внедорожника T2.",
+     "description": "Jetour в Китае начал продажи гибридной версии внедорожника T2.",
+     "image": "https://img.gazeta.ru/files3/161/13343161/20200823_zaa_p133_059-pic905-895x505-93970.jpg",
+     "source_id": 15,
+     "category_id": 4
+   },
+   {
+     "id": 87866,
+     "lang": "ru",
+     "date": "Mon Oct 11 2021 16:37:02 GMT+0300 (Moscow Standard Time)",
+     "title": "Мы откатились в 90-е: «Лиса рулит» о китайских автомобилях и сером импорте",
+     "description": "Мы откатились в 90-е: «Лиса рулит» о китайских автомобилях и сером импорте",
+     "image": "https://img.gazeta.ru/files3/773/13274773/RIAN_6109420.HR-pic905-895x505-55906.jpg",
+     "source_id": 15,
+     "category_id": 2
+   },
+   {
+     "id": 87877,
+     "lang": "ru",
+     "date": "Mon Oct 11 2021 16:35:56 GMT+0300 (Moscow Standard Time)",
+     "title": "Автомобили китайской Chery получат навигацию от «Яндекса». ",
+     "description": "Автомобили китайской Chery получат навигацию от «Яндекса». ",
+     "image": "https://www.buro247.ru/images/andreeva/670x340_vag1110.jpg",
+     "source_id": 10,
+     "category_id": 3
+   },
+   {
+     "id": 87882,
+     "lang": "ru",
+     "date": "Mon Oct 11 2021 16:35:03 GMT+0300 (Moscow Standard Time)",
+     "title": "Geely обновила популярный в России кроссовер Monjaro. ",
+     "description": "Geely обновила популярный в России кроссовер Monjaro. ",
+     "image": "https://www.buro247.ru/local/share/images/81784.JPG",
+     "source_id": 10,
+     "category_id": 3
+   },
+   {
+     "id": 87886,
+     "lang": "ru",
+     "date": "Mon Oct 11 2021 16:34:19 GMT+0300 (Moscow Standard Time)",
+     "title": "«АвтоВАЗ» в марте побил 11-летний рекорд по продажам.",
+     "description": "«АвтоВАЗ» в марте побил 11-летний рекорд по продажам.",
+     "image": "https://aif-s3.aif.ru/images/025/372/d627b324d6598fc5f64ff0b4a31ca518.jpg",
+     "source_id": 14,
+     "category_id": 5
+   },
+   {
+     "id": 87871,
+     "lang": "ru",
+     "date": "Mon Oct 11 2021 16:33:00 GMT+0300 (Moscow Standard Time)",
+     "title": "Продажи флагманского кроссовера Jaecoo в России стартуют в апреле.",
+     "description": "Продажи флагманского кроссовера Jaecoo в России стартуют в апреле.",
+     "image": "https://cdn.igromania.ru/mnt/news/c/6/1/7/a/0/109637/6234e86b4f8ebc92_848x477.jpg",
+     "source_id": 9,
+     "category_id": 1
+   },
+   {
+     "id": 87905,
+     "lang": "ru",
+     "date": "Mon Oct 11 2021 16:31:56 GMT+0300 (Moscow Standard Time)",
+     "title": "Продажи нового Haval H3 стартуют в России во втором квартале 2024 года.",
+     "description": "Продажи нового Haval H3 стартуют в России во втором квартале 2024 года.",
+     "image": "https://img.gazeta.ru/files3/941/12068941/RIAN_3288385.HR-pic905-895x505-86012.jpg",
+     "source_id": 15,
+     "category_id": 4
+   },
+   {
+     "id": 87925,
+     "lang": "ru",
+     "date": "Mon Oct 11 2021 16:29:00 GMT+0300 (Moscow Standard Time)",
+     "title": "Назван топ-5 самых продаваемых автомобилей в России.",
+     "description": "Назван топ-5 самых продаваемых автомобилей в России.",
+     "image": "https://3dnews.ru/assets/external/illustrations/2021/10/11/1051016/doublefine.jpg",
+     "source_id": 6,
+     "category_id": 1
+   },
+   {
+     "id": 87919,
+     "lang": "ru",
+     "date": "Mon Oct 11 2021 16:29:00 GMT+0300 (Moscow Standard Time)",
+     "title": "В России резко подорожали машины по параллельному импорту.",
+     "description": "В России резко подорожали машины по параллельному импорту.",
+     "image": "https://3dnews.ru/assets/external/illustrations/2021/10/11/1051011/603fe3c1e3.jpg",
+     "source_id": 6,
+     "category_id": 1
+   },
+   {
+     "id": 87884,
+     "lang": "ru",
+     "date": "Mon Oct 11 2021 16:28:35 GMT+0300 (Moscow Standard Time)",
+     "title": "Xiaomi начала поставки своего первого электрокара SU7. ",
+     "description": "Xiaomi начала поставки своего первого электрокара SU7. ",
+     "image": "",
+     "source_id": 1,
+     "category_id": 4
+   },
+   {
+     "id": 87907,
+     "lang": "ru",
+     "date": "Mon Oct 11 2021 16:28:01 GMT+0300 (Moscow Standard Time)",
+     "title": "Omoda C5 в базовой комплектации подешевела на ₽500 тыс.",
+     "description": "Omoda C5 в базовой комплектации подешевела на ₽500 тыс.",
+     "image": "https://img.gazeta.ru/files3/430/12928430/RIAN_5615942.HR-pic905-895x505-27387.jpg",
+     "source_id": 15,
+     "category_id": 4
+   }
+ ]
+}
+
+
+const escapeString = (string) => { 
+const tagsToReplace = { 
+'&': '&amp;', 
+'<': '&lt;', 
+'>': '&gt;' 
+}; 
+
+return string.replace(/[&<>]/g, function(tag) { 
+return tagsToReplace[tag] || tag; 
+}); 
+} 
+
+const createMainNewsItem = (item) => { 
+return ` 
+<article class="main-article"> 
+<div class="main-article__image-container"> 
+<img class="article-img main-article__image" src="${encodeURI(item.image)}" alt="Фото новости"> 
+</div> 
+<div class="main-article__content"> 
+<span class="article-category">${escapeString(data.categories.find(({ id }) => item.category_id === id).name)}</span> 
+<h2 class="main-article__title">${escapeString(item.title)}</h2> 
+<p class="main-article__text">${escapeString(item.description)}</p> 
+<span class="article-source main-article__caption">${escapeString(data.sources.find(({ id }) => item.source_id === id).name)}</span> 
+</div> 
+</article> 
+`; 
+} 
+
+const createSmallNewsItem = (item) => { 
+return ` 
+<article class="small-article"> 
+<h2 class="small-articlle__title">${escapeString(item.title)}</h2> 
+<span class="article-date">${escapeString(data.sources.find(({ id }) => item.source_id === id).name)}</span> 
+<span class="article-source">${escapeString(new Date(item.date).toLocaleDateString('ru-RU', { month: 'long', day: 'numeric' }))}</span> 
+</article> 
+`; 
+} 
+
+const mainNews = data.items.slice(0, 5); 
+const mainNewsContainer = document.querySelector('.articles__big-column'); 
+
+mainNews.forEach((item) => { 
+const template = document.createElement('template'); 
+template.innerHTML = createMainNewsItem(item); 
+mainNewsContainer.appendChild(template.content); 
+}) 
+
+const smallNews = data.items.slice(3, 12); 
+const smallNewsContainer = document.querySelector('.articles__small-column'); 
+
+smallNews.forEach((item) => { 
+const template = document.createElement('template'); 
+template.innerHTML = createSmallNewsItem(item); 
+smallNewsContainer.appendChild(template.content); 
+})
